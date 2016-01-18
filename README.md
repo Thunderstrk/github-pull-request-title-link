c6bcf4984fc78d84bd1b514a88211f0d65bb7789
Dustin Ryerson <dustin.ryerson@liferay.com>
2016-01-17 21:09:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GitHub Pull Request Title Link
+
+Changes pull requests titles that reference a JIRA ticket to a link.
+
+## Installation
+
+1. Clone or fork the repo
+2. Go to chrome://extensions/
+3. Check the "Developer Mode" box
+4. Click "Load Unpacked Extension"
+5. Select repo folder
---
+++ github-title-link.js
@@ -0,0 +1,11 @@
+var titles = [].concat(
+    Array.prototype.slice.call(document.getElementsByClassName('js-issue-title'))
+).concat(
+    Array.prototype.slice.call(document.getElementsByClassName('commit-title'))
+);
+
+titles.forEach(
+    function (title) {
+        title.innerHTML = title.textContent.replace(/([A-Z]+-[0-9]+)/, '<a href="https://issues.liferay.com/browse/$1">$1</a>');
+    }
+);
