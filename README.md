43f8c834319e22baad99f3ce4f49ad0c90d503b7
Alexandre Billay <a.billay@pc355.na.corp.samsungelectronics.net>
2021-06-16 14:54:41 -0400

    Add Firefox Developers support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Changes pull requests titles that reference a JIRA ticket to a link.
 
-## Installation
+## Installation on Chrome
 
 1. Clone or fork the repo
 2. Edit github-title-link.js and replace "issues.liferay.com" with your JIRA domain.
@@ -10,3 +10,13 @@ Changes pull requests titles that reference a JIRA ticket to a link.
 4. Check the "Developer Mode" box
 5. Click "Load Unpacked Extension"
 6. Select repo folder
+
+## Installation on Firefox Developers
+
+1. Clone or fork the repo
+2. Edit the domain for our JIRA
+3. Install web-ext (npm install --global web-ext)
+4. web-ext build at the root of the project
+5. Enable unsigned extesion on Firefox Dev (about:config then set xpinstall.signatures.required to false)
+6. Go to extensions page on Firefox Dev settings
+7. Drag and Drop the built extension zip
--- github-title-link.js
+++ github-title-link.js
@@ -1,11 +1,16 @@
-var titles = [].concat(
-    Array.prototype.slice.call(document.getElementsByClassName('js-issue-title'))
-).concat(
-    Array.prototype.slice.call(document.getElementsByClassName('commit-title'))
-);
+var titles = []
+  .concat(
+    Array.prototype.slice.call(
+      document.getElementsByClassName("js-issue-title")
+    )
+  )
+  .concat(
+    Array.prototype.slice.call(document.getElementsByClassName("commit-title"))
+  );
 
-titles.forEach(
-    function (title) {
-        title.innerHTML = title.textContent.replace(/([A-Z]+-[0-9]+)/g, '<a href="https://issues.liferay.com/browse/$1" target="_blank">$1</a>');
-    }
-);
+titles.forEach(function (title) {
+  title.innerHTML = title.textContent.replace(
+    /([A-Z]+-[0-9]+)/g,
+    '<a href="https://issues.liferay.com/browse/$1" target="_blank">$1</a>'
+  );
+});
